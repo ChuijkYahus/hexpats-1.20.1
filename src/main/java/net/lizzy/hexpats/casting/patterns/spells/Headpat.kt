3a83e15065e83f4ab725c7d5120578c3c5d856dd
TallestMidget <75638509+TallestMidget@users.noreply.github.com>
2025-04-04 23:05:18 -0700

    Fixed error with headpat spell that happens when the hex is done by a null caster.

--- src/main/java/net/lizzy/hexpats/casting/patterns/spells/Headpat.kt
+++ src/main/java/net/lizzy/hexpats/casting/patterns/spells/Headpat.kt
@@ -1,6 +1,5 @@
 package net.lizzy.hexpats.casting.patterns.spells
 
-import at.petrak.hexcasting.api.casting.ParticleSpray
 import at.petrak.hexcasting.api.casting.RenderedSpell
 import at.petrak.hexcasting.api.casting.castables.SpellAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
@@ -9,6 +8,7 @@ import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.paucal.common.ModStats
 import at.petrak.paucal.common.misc.PatPat
 import net.minecraft.entity.Entity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.sound.SoundCategory
@@ -24,9 +24,11 @@ class Headpat : SpellAction {
     private data class Spell(val target: Entity) : RenderedSpell {
         override fun cast(env: CastingEnvironment) {
             val caster = env.castingEntity
-            val player = env.world.getPlayerByUuid(caster!!.uuid)
             val world = env.world
-
+            var player : PlayerEntity? = null
+            if (caster != null) {
+                player = env.world.getPlayerByUuid(caster.uuid)
+            }
             if (world is ServerWorld) {
                 val pos = target.eyePos
                 world.spawnParticles(ParticleTypes.HEART, pos.x, pos.y + 0.5, pos.z, 1, 0.0, 0.0, 0.0, 0.1)
